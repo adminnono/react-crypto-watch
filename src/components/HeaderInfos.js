773e58b01ed5c42a1e502a25214ab6658f84fdb4
arnaud <arnaud.dujardin76@gmail.com>
2025-07-15 11:58:22 +0200

    Ajout composant HeaderInfos et PercentChange

---
+++ src/components/HeaderInfos.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const HeaderInfos = () => {
+  const [headerData, setHeaderData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/global")
+      .then((res) => setHeaderData(res.data.data));
+  }, []);
+
+  return (
+    <div className="header-container">
+      <ul className="title">
+        <li>
+          <h1>
+            <img src="./assets/logo.png" alt="logo" />
+            Watch Tower
+          </h1>
+        </li>
+        <li>
+          Crypto-monnaies :{" "}
+          {headerData.active_cryptocurrencies &&
+            headerData.active_cryptocurrencies.toLocaleString()}
+        </li>
+        <li>
+          Marchés : {headerData.markets && headerData.markets.toLocaleString()}
+        </li>
+      </ul>
+      <ul className="infos-mkt">
+        <li className="global-mkt">
+          Global Market Cap : <strong></strong>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default HeaderInfos;
---
+++ src/components/PercentChange.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PercentChange = () => {
+  return <div></div>;
+};
+
+export default PercentChange;
