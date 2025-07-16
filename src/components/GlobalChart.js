2a7819b92e2c7840bb5f04802068f0462e3e1659
arnaud <arnaud.dujardin76@gmail.com>
2025-07-16 15:06:45 +0200

    Ajout treemap params

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Treemap } from "recharts";
 
 const GlobalChart = ({ coinsData }) => {
+  const [dataArray, setDataArray] = useState([]);
+  useEffect(() => {
+    let chartData = [];
+
+    if (coinsData.length > 0) {
+      for (let i = 0; i < 45; i++) {
+        chartData.push({
+          name:
+            coinsData[i].symbol.toUpperCase() +
+            " " +
+            coinsData[i].market_cap_change_percentage_24h.toFixed(1) +
+            "%",
+          size: coinsData[i].market_cap,
+          fill: null,
+        });
+      }
+    }
+    setDataArray(chartData);
+  }, [coinsData]);
+
   return (
     <div className="global-chart">
-      <h1>Test</h1>
+      <Treemap
+        width={730}
+        height={181}
+        data={dataArray}
+        dataKey="size"
+        stroke="rgb(51,51,51)"
+        fill="black"
+        aspectRatio="1"
+      />
     </div>
   );
 };
