c135bbb188a45daa7e59dae8f6425534a7e4a4be
arnaud <arnaud.dujardin76@gmail.com>
2025-07-16 23:35:03 +0200

    Ajout tooltip et couleurs dynamiques sur la trimap

--- src/components/GlobalChart.js
+++ src/components/GlobalChart.js
@@ -1,8 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Treemap } from "recharts";
+import { Tooltip, Treemap } from "recharts";
+import colors from "../styles/_settings.scss";
 
 const GlobalChart = ({ coinsData }) => {
   const [dataArray, setDataArray] = useState([]);
+
+  const colorPicker = (number) => {
+    if (number >= 20) {
+      return colors.color1;
+    } else if (number >= 5) {
+      return colors.green2;
+    } else if (number >= 0) {
+      return colors.green1;
+    } else if (number >= -5) {
+      return colors.red1;
+    } else if (number >= -20) {
+      return colors.red2;
+    } else {
+      return colors.black2;
+    }
+  };
+
+  const excludeCoin = (coin) => {
+    if (
+      coin === "usdt" ||
+      coin === "usdc" ||
+      coin === "busd" ||
+      coin === "dai" ||
+      coin === "ust" ||
+      coin === "mim"
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   useEffect(() => {
     let chartData = [];
 
@@ -15,12 +48,23 @@ const GlobalChart = ({ coinsData }) => {
             coinsData[i].market_cap_change_percentage_24h.toFixed(1) +
             "%",
           size: coinsData[i].market_cap,
-          fill: null,
+          fill: colorPicker(coinsData[i].price_change_percentage_24h),
         });
       }
     }
     setDataArray(chartData);
   }, [coinsData]);
+
+  const tremapToolTip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{payload[0].payload.name}</p>
+        </div>
+      );
+    }
+    return null;
+  };
 
   return (
     <div className="global-chart">
@@ -32,7 +76,9 @@ const GlobalChart = ({ coinsData }) => {
         stroke="rgb(51,51,51)"
         fill="black"
         aspectRatio="1"
-      />
+      >
+        <Tooltip content={tremapToolTip} />
+      </Treemap>
     </div>
   );
 };
