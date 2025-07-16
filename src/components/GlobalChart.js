7d1207adb7a575b031976e6d295cb3a7e30131a9
arnaud <arnaud.dujardin76@gmail.com>
2025-07-16 12:44:30 +0200

    Ajout composants boutons, carte trimap et pourcentage marché avec variables couleurs dynamiques

---
+++ src/components/GlobalChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GlobalChart = ({ coinsData }) => {
+  return (
+    <div className="global-chart">
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default GlobalChart;
--- src/components/HeaderInfos.js
+++ src/components/HeaderInfos.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import PercentChange from "./PercentChange";
+import TableFilters from "./TableFilters";
 
 const HeaderInfos = () => {
   const [headerData, setHeaderData] = useState([]);
@@ -30,9 +32,23 @@ const HeaderInfos = () => {
       </ul>
       <ul className="infos-mkt">
         <li className="global-mkt">
-          Global Market Cap : <strong></strong>
+          Global Market Cap :
+          <PercentChange
+            percent={headerData.market_cap_change_percentage_24h_usd}
+          />
+        </li>
+        <li>
+          BTC dominance :{" "}
+          {headerData.market_cap_percentage &&
+            headerData.market_cap_percentage.btc.toFixed(1) + "%"}
+        </li>
+        <li>
+          ETH dominance :{" "}
+          {headerData.market_cap_percentage &&
+            headerData.market_cap_percentage.eth.toFixed(1) + "%"}
         </li>
       </ul>
+      <TableFilters />
     </div>
   );
 };
