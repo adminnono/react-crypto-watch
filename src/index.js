1047fcb45376045dcba517f2546785fed2229589
arnaud <arnaud.dujardin76@gmail.com>
2025-07-20 12:12:31 +0200

    Ajout redux et reducers

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
