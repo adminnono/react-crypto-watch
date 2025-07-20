28af43962262d24b4986fa149fca5029b146c640
arnaud <arnaud.dujardin76@gmail.com>
2025-07-20 23:56:32 +0200

    Ajout avec ou sans favoris

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,6 +8,7 @@ const Table = ({ coinsData }) => {
   const [orderBy, setOrderBy] = useState("");
   const [rangeNumber, setRangeNumber] = useState(100);
   const showStable = useSelector((state) => state.stableReducer.showStable);
+  const showFavList = useSelector((state) => state.listReducer.showList);
 
   const tableHeader = [
     "Prix",
@@ -74,6 +75,16 @@ const Table = ({ coinsData }) => {
               if (isStableCoin(coin.symbol)) {
                 return coin;
               }
+            }
+          })
+          .filter((coin) => {
+            if (showFavList) {
+              let list = window.localStorage.coinList.split(",");
+              if (list.includes(coin.id)) {
+                return coin;
+              }
+            } else {
+              return coin;
             }
           })
           .sort((a, b) => {
--- src/components/TableFilters.js
+++ src/components/TableFilters.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setStableState } from "../action/stable.action";
+import { setListDisplay } from "../action/list.action";
 
 const TableFilters = () => {
   const [showStable, setShowStable] = useState(true);
+  const [showFavList, setShowFavList] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setStableState(showStable));
-  }, [showStable]);
+    dispatch(setListDisplay(showFavList));
+  }, [showStable, showFavList]);
 
   return (
     <div className="table-filters">
@@ -24,10 +27,16 @@ const TableFilters = () => {
             {showStable ? "Avec stable coin" : "Sans stable coin"}
           </label>
         </div>
-        <div className="no-list-btn">
+        <div
+          className={showFavList ? "no-list-btn" : "no-list-btn active"}
+          onClick={() => setShowFavList(false)}
+        >
           <p>Aucune liste</p>
         </div>
-        <div className="fav-list">
+        <div
+          className={showFavList ? "fav-list active" : "fav-list"}
+          onClick={() => setShowFavList(true)}
+        >
           <p>Liste des favoris</p>
           <img src="./assets/star-full.svg" alt="icon-star" />
         </div>
